5c375e9a5bb967872cae85fd130983c6048311fb
Jaime Rivas <jaimerivas81@outlook.com>
2019-10-09 21:38:21 -0700

    Main menu scene added; cinemachine vcam looks at AI bot from a distance; menus can navigate between options, credits, and main menu; start button starts game; quit button quits application

---
+++ Assets/Scripts/Main Menu/GoToOptionsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class GoToOptionsMenu : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+
+    private float optionsCameraPathPos = 1;
+
+    public void MoveCameraAndSwitchMenus()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Application");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Main Menu/PatrolArea.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolArea : MonoBehaviour
+{
+    private NavMeshAgent thisAgent;
+
+    [SerializeField] private float patrolRadius = 100.0f;
+    [SerializeField] private float patrolTimer = 10.0f;
+
+    private float timerCount;
+
+    private void Awake()
+    {
+        thisAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        timerCount = patrolTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        timerCount += Time.deltaTime;
+        if(timerCount >= patrolTimer)
+        {
+            SetNewRandomDest();
+            timerCount = 0f;
+        }
+    }
+
+    private void SetNewRandomDest()
+    {
+        Vector3 newDest = RandomNavSphere(transform.position, patrolRadius, -1);
+        thisAgent.SetDestination(newDest);
+    }
+
+    private Vector3 RandomNavSphere(Vector3 origin, float patrolRadius, int layerMask)
+    {
+        Vector3 randDir = UnityEngine.Random.insideUnitSphere * patrolRadius;
+        randDir += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDir, out navHit, patrolRadius, layerMask);
+
+        return navHit.position;
+    }
+}
