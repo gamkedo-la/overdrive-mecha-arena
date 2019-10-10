287eb86c19d534d63a1d5d8b971db83d3615ee67
Jaime Rivas <jaimerivas81@outlook.com>
2019-10-09 22:03:59 -0700

    Virtual cam navigates to a different position depending on the menu context

---
+++ Assets/Scripts/Main Menu/HandleMenuSelections.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class HandleMenuSelections : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+
+    private float optionsCameraPathPos = 1;
+
+    public void MoveCameraAndSwitchToOptionsMenu()
+    {
+        CinemachineTrackedDolly vcamTrackedDolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        vcamTrackedDolly.m_PathPosition = 1;
+    }
+    public void MoveCameraAndSwitchToCreditsMenu()
+    {
+        CinemachineTrackedDolly vcamTrackedDolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        vcamTrackedDolly.m_PathPosition = 2;
+    }
+    public void MoveCameraAndSwitchToMainMenu()
+    {
+        CinemachineTrackedDolly vcamTrackedDolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        vcamTrackedDolly.m_PathPosition = 0;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Application");
+        Application.Quit();
+    }
+}
