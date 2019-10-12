3157ecc738adce9d0223eb003982df0b81b9c279
Jaime Rivas <jaimerivas81@outlook.com>
2019-10-11 20:30:41 -0700

    Dangerous object demo; Health UI reworked to function properly with UI programming (not functional yet)

---
+++ Assets/DamageMechaIfCollided.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageMechaIfCollided : MonoBehaviour
+{
+    private Rigidbody beamRB;
+
+    [SerializeField] private int beamDamage = 20;
+    [SerializeField] private float fallRate = 20.0f;
+
+    void Start()
+    {
+        beamRB = GetComponent<Rigidbody>();
+        Physics.IgnoreLayerCollision(12, 13);
+    }
+    private void Update()
+    {
+        //add downward force to beam
+        beamRB.AddForce(-transform.up * fallRate);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //first make sure to ignore trigger
+        //check if other collider/rigidbody has health and if our beam's y pos is greater than the mechas (y pos + their mesh height)
+        //if those are true then we know it can be damaged so call their TakeDamage method
+        //now destroy the beam GO
+        //else do nothing and destroy the beam GO since it's useless
+    }
+}
---
+++ Assets/DropBeamAtRandomPos.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBeamAtRandomPos : MonoBehaviour
+{
+    private GameObject beamPrefab;
+
+    [SerializeField] private float beamDropTimer = 30.0f;
+    private float dropCount;
+    private bool canDropBeam = false;
+
+    void Awake()
+    {
+        beamPrefab = Resources.Load("Beam") as GameObject;
+    }
+
+    private void Start()
+    {
+        Debug.Log(beamPrefab);
+    }
+
+    private void Update()
+    {
+        dropCount += Time.deltaTime;
+        if(dropCount >= beamDropTimer)
+        {
+            canDropBeam = true;
+            dropCount = 0f;
+        }
+        else
+        {
+            canDropBeam = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //get random vector3 for beam's spawn location
+        //ensure y pos is roughly the same as the trigger zone's height
+        //spawn prefab at location within trigger zone
+        if (canDropBeam)
+        {
+            Instantiate(beamPrefab);
+        }
+        else
+        {
+            Debug.Log("Cannot drop a beam!");
+        }
+    }
+}
