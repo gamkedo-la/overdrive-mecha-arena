9daa724dacb1431df1e0daf362fac7a518ce1c67
Gamkedo <chris@gamkedo.com>
2019-10-30 15:57:35 -0500

    Code added for helicopter to idly change altitude in a range, and for tank to scoot back and forth within a fixed range, just so they're no longer static in the environment

---
+++ Assets/Scripts/Enemy/HeliMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliMovement : MonoBehaviour
+{
+    private float altitudeChangeSpeed = 2.4f;
+    private float altChangeRange = 8.0f;
+    private float hoverHeightTarget;
+
+    private float minAlt, maxAlt; // calculated at start from position +/- altChangeRange
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minAlt = transform.position.y - altChangeRange;
+        maxAlt = transform.position.y + altChangeRange;
+        hoverHeightTarget = transform.position.y;
+        StartCoroutine(AIChangeMind());
+    }
+
+    IEnumerator AIChangeMind()
+    {
+        while(true)
+        {
+            float minMove = Random.Range(0.4f, 1.0f) * 7.0f;
+            if(Random.Range(0.0f,1.0f)<0.5f)
+            {
+                minMove *= -1.0f;
+            }
+            hoverHeightTarget += minMove;
+            hoverHeightTarget = Mathf.Clamp(hoverHeightTarget, minAlt, maxAlt);
+
+            yield return new WaitForSeconds( Random.Range(0.4f,1.5f) );
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position-Vector3.up* altChangeRange, transform.position + Vector3.up * altChangeRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hoverHeightTarget < transform.position.y - altitudeChangeSpeed)
+        {
+            transform.position -= Vector3.up * altitudeChangeSpeed * Time.deltaTime;
+        } else if (hoverHeightTarget > transform.position.y + altitudeChangeSpeed)
+        {
+            transform.position += Vector3.up * altitudeChangeSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/TankMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    private float driveSpeed = 2.4f;
+    private float driveRange = 8.0f;
+    private float linePercGoal = 0.5f;
+    private Vector3 driveToPtTarget;
+    public GameObject debugNodeVis;
+
+    private Vector3 minPos, maxPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minPos = transform.position - driveRange * transform.forward;
+        maxPos = transform.position + driveRange * transform.forward;
+        driveToPtTarget = transform.position;
+        StartCoroutine(AIChangeMind());
+    }
+
+    private void updateDriveDest()
+    {
+        driveToPtTarget = Vector3.Lerp(minPos, maxPos, linePercGoal);
+        debugNodeVis.transform.position = driveToPtTarget;
+    }
+
+    IEnumerator AIChangeMind()
+    {
+        while (true)
+        {
+            float minMove = Random.Range(0.4f, 1.0f) * 0.4f;
+            if (Random.Range(0.0f, 1.0f) < 0.5f)
+            {
+                minMove *= -1.0f;
+            }
+            linePercGoal += minMove;
+            linePercGoal = Mathf.Clamp01(linePercGoal);
+
+            updateDriveDest();
+
+            yield return new WaitForSeconds(Random.Range(0.4f, 1.5f));
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position - transform.forward * driveRange, transform.position + transform.forward * driveRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedDir;
+        float angleToDriveGoal = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(driveToPtTarget - transform.position));
+        Debug.Log(angleToDriveGoal);
+        if ( Vector3.Distance(transform.position, driveToPtTarget) < driveSpeed)
+        {
+            speedDir = 0.0f;
+        } else if (angleToDriveGoal < 90.0f)
+        {
+            speedDir = 1.0f;
+        }
+        else
+        {
+            speedDir = -1.0f;
+        }
+        transform.position += transform.forward * driveSpeed * Time.deltaTime * speedDir;
+    }
+}
