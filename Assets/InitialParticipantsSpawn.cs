c7ff07fddd2b1674b5ae80c056856084786277b2
Jaime Rivas <jaimerivas81@outlook.com>
2019-11-04 17:29:50 -0800

    Prefab variants based off basic mech prefab (enemy mechs); initial code for multiple enemy support and spawning in battle arena

---
+++ Assets/InitialParticipantsSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialParticipantsSpawn : MonoBehaviour
+{
+    [SerializeField] private int numberOfParticipants = 8;
+    [SerializeField] private List<GameObject> spawnPointsList;
+    [SerializeField] private List<GameObject> enemyCharacters;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
