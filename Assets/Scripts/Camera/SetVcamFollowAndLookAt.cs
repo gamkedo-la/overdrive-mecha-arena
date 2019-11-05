933f5cfc6d67477e2da6f14cc028347142b8d53a
Jaime Rivas <jaimerivas81@outlook.com>
2019-11-04 19:34:42 -0800

    Organized scripts into appropriate folders; AI mechas variants spawn at random spawn point; player spawns at random point (spawning for all mechs is buggy; currently there's is no code preventing spawning mechs on top of each other); vcam follow and look at values set whe player spawn (again behavior is really buggy likely due to the spawn issue)

---
+++ Assets/Scripts/Camera/SetVcamFollowAndLookAt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SetVcamFollowAndLookAt : MonoBehaviour
+{
+    [SerializeField] private GameObject VcamGO;
+    [SerializeField] private Transform camLookAt;
+    private CinemachineVirtualCamera vcamScript;
+
+    private void Awake()
+    {
+        VcamGO = GameObject.Find("CM vcam1");
+        vcamScript = VcamGO.GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Start()
+    {
+        vcamScript.Follow = transform;
+        vcamScript.LookAt = camLookAt;
+    }
+}
--- Assets/Scripts/Spawn_Respawn/InitialParticipantsSpawn.cs
+++ Assets/Scripts/Spawn_Respawn/InitialParticipantsSpawn.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 
 public class InitialParticipantsSpawn : MonoBehaviour
 {
-    [SerializeField] private int numberOfParticipants = 8;
+    [SerializeField] private int numberOfAIParticipants = 7;
     [SerializeField] private List<GameObject> spawnPointsList;
     [SerializeField] private List<GameObject> enemyCharacters;
     [SerializeField] private GameObject playerMech;
 
     void Start()
     {
-        for (int i = 0; i < numberOfParticipants; i++)
+        for (int i = 0; i < numberOfAIParticipants; i++)
         {
             Transform spawnPoint = SelectSpawnPoint();
             SpawnMechAt(spawnPoint);
         }
 
-        //SpawnPlayer();
+        SpawnPlayer();
     }
 
-    void Update()
+    private void SpawnPlayer()
     {
-        //Debug.Log(spawnPointsList.Count);
+        Transform spawnPoint = SelectSpawnPoint();
+        Instantiate(playerMech, spawnPoint);
     }
 
     private void SpawnMechAt(Transform spawnPoint)
@@ -34,6 +35,7 @@ public class InitialParticipantsSpawn : MonoBehaviour
         Instantiate(mechToSpawn, spawnPoint);
     }
 
+    //TODO: use one spawn point per mech to avoid mechs spawning on top of each other
     private Transform SelectSpawnPoint()
     {
         int index = UnityEngine.Random.Range(0, spawnPointsList.Count);
