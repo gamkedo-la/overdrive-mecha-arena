0c4fb03c3663d85a1dbda383fe7f58e40a150dff
Jaime Rivas <jaimerivas81@outlook.com>
2019-11-30 23:17:39 -0800

    Laser fence hurts mech when passing through; Barely functional tesla damage code (can seem to figure out how to use Coroutines for a pulsating effect)

---
+++ Assets/Scripts/General Gameplay/LaserFenceHazard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserFenceHazard : MonoBehaviour
+{
+    [SerializeField] private int damage = 20;
+
+    void Start()
+    {
+        //ignore collision between dangerous objects and triggers
+        Physics.IgnoreLayerCollision(12, 13);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health mechaHealth = other.gameObject.GetComponent<Health>();
+
+        if (mechaHealth != null)
+        {
+            Debug.Log("Mecha hit by dangerous object!");
+            mechaHealth.TakeDamage(damage);
+        }
+        else
+        {
+            Debug.Log("Collider that made contact with me does not have a Health script!");
+        }//end of placeholder code
+    }
+}
---
+++ Assets/Scripts/General Gameplay/TeslaHazard.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeslaHazard : MonoBehaviour
+{
+    [SerializeField] private int damage = 20;
+    [SerializeField] private float timeBetweenElectricalSurges = 5.0f;
+    [SerializeField] private float surgeRadius = 40.0f;
+    [SerializeField] private float normalRadius = 10.0f;
+    ParticleSystem ps;
+
+    private bool canDischargePower = false;
+
+    void Start()
+    {
+        ps = GetComponentInParent<ParticleSystem>();
+        Physics.IgnoreLayerCollision(12, 13);
+
+        StartCoroutine(waitBeforeSurging());
+    }
+
+    void Update()
+    {
+        if(canDischargePower)
+        {
+            canDischargePower = false;
+            ParticleSystem.ShapeModule shape = ps.shape;
+            shape.radius = surgeRadius;
+        }
+        else
+        {
+            ParticleSystem.ShapeModule shape = ps.shape;
+            shape.radius = normalRadius;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(canDischargePower)
+        {
+            Health mechaHealth = other.gameObject.GetComponent<Health>();
+
+            if (mechaHealth != null)
+            {
+                Debug.Log("Mecha hit by dangerous object!");
+                mechaHealth.TakeDamage(damage);
+            }
+            else
+            {
+                Debug.Log("Collider that made contact with me does not have a Health script!");
+            }
+        }
+        else
+        {
+            // Could do something cool like maybe give a damage boost to the mechs that stay withing danger range
+        }
+    }
+
+    IEnumerator waitBeforeSurging()
+    {
+        yield return new WaitForSeconds(timeBetweenElectricalSurges);
+        canDischargePower = true;
+    }
+}
