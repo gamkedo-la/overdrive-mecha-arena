bbf835dfefec5e9a942aaf1f996597bf2118f0ec
Jaime Rivas <jaimerivas81@outlook.com>
2019-11-30 22:11:20 -0800

    Time limit for battles implemented; Player sent back to main menu when battle ends (temporary until we get a Results Scene)

--- Assets/Scripts/General Gameplay/StartAndEndMatch.cs
+++ Assets/Scripts/General Gameplay/StartAndEndMatch.cs
@@ -2,20 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
 
 public class StartAndEndMatch : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI tmProText;
+    [SerializeField] private float timeLimit;
+
+    private float timer;
+    private bool allowCountdown = true;
+    private bool countdownOnce = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = timeLimit;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer >= 0.0f && allowCountdown)
+        {
+            timer -= Time.deltaTime;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(timer);
+
+            tmProText.text = timeSpan.ToString("mm':'ss");
+        }
+        else if(timer <= 0.0f && !countdownOnce)
+        {
+            allowCountdown = false;
+            countdownOnce = true;
+
+            timer = 0.0f;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(timer);
+            tmProText.text = timeSpan.ToString("mm':'ss");
+
+            GameOver();
+        }
+    }
+
+    public void ResetTimeLimit()
+    {
+        timer = timeLimit;
+        allowCountdown = true;
+        countdownOnce = false;
+    }
+
+    private void GameOver()
+    {
+        // 0 is the Main Menu scene and should never be changed
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(0);
     }
 }
