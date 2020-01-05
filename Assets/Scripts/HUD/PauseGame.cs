e81cf911fca8e0de727eeff57069febf4e982f75
Jaime Rivas <jaimerivas81@outlook.com>
2020-01-05 10:11:15 -0800

    Initial pause menu code

---
+++ Assets/Scripts/HUD/PauseGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool GameIsPaused;
+    public GameObject pauseUI;
+    public GameObject gameUI;
+
+    private void Update()
+    {
+        if(Input.GetButton("Pause"))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        gameUI.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        gameUI.SetActive(false);
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+
+    }
+
+    public void Quit()
+    {
+
+    }
+}
