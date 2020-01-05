b73dd5416ca952c17277dd8487eca1a0b5a9335d
Jaime Rivas <jaimerivas81@outlook.com>
2020-01-05 10:39:23 -0800

    Pause menu basic functionality complete (audio stuff not hooked up yet);

--- Assets/Scripts/HUD/PauseGame.cs
+++ Assets/Scripts/HUD/PauseGame.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class PauseGame : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetButton("Pause"))
+        if(Input.GetButtonDown("Pause"))
         {
             if(GameIsPaused)
             {
@@ -26,6 +27,8 @@ public class PauseGame : MonoBehaviour
     }
     public void Resume()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
         pauseUI.SetActive(false);
         gameUI.SetActive(true);
         Time.timeScale = 1f;
@@ -34,6 +37,8 @@ public class PauseGame : MonoBehaviour
 
     private void Pause()
     {
+        Cursor.lockState = CursorLockMode.None;
+
         gameUI.SetActive(false);
         pauseUI.SetActive(true);
         Time.timeScale = 0f;
@@ -42,11 +47,12 @@ public class PauseGame : MonoBehaviour
 
     public void LoadMenu()
     {
-
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
     }
 
     public void Quit()
     {
-
+        Application.Quit();
     }
 }
