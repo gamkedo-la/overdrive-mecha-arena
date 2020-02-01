f2566d03fea2029dacedb7416aa16811c78e057b
Jaime Rivas <jaimerivas81@outlook.com>
2020-01-31 21:02:20 -0800

    AI mechs spawn with semi-unique names

---
+++ Assets/Scripts/Enemy/SetName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetName : MonoBehaviour
+{
+    [SerializeField] private Mecha mech;
+
+    private void Start()
+    {
+        gameObject.name = mech.names[Random.Range(0, mech.names.Count - 1)];
+    }
+}
