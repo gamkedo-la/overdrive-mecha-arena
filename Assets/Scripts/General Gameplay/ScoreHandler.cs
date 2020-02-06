56ff14ad95c5a24b641351cc30070a9f5c413384
Jaime Rivas <jaimerivas81@outlook.com>
2020-02-05 18:20:28 -0800

    Further work on scoring system

--- Assets/Scripts/General Gameplay/ScoreHandler.cs
+++ Assets/Scripts/General Gameplay/ScoreHandler.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class ScoreHandler : MonoBehaviour
 {
+    [SerializeField] private ScoreKeeper scoreKeeper;
     private Health health;
 
     private float score = 0;
-    private float kills = 0;
-    private float deaths = 0;
+    private float totalKills = 0;
+    private float totalDeaths = 0;
 
-    private float killstreakBonus = 1.5f;
+    private bool isFirstKillInStreak = false;
+    public bool _isFirtstKillInStreak { set { isFirstKillInStreak = value; } }
+
+    private float initialKillstreakBonus = 1.5f;
+    private float currentKillstreakBonus = 0.0f;
+
     private float deathPenalty;
+    private float deathPenaltyMultiplier = 0.66f;
+
+    private void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
 
     private void Start()
     {
+        if (!scoreKeeper._participants.Contains(this))
+        {
+            scoreKeeper._participants.Add(this);
+        }
+
         health = GetComponent<Health>();
         deathPenalty = health.getBaseHP;
+    }
+
+    public void AddToScore(float damage)
+    {
+        
     }
 }
--- Assets/Scripts/General Gameplay/ScoreKeeper.cs
+++ Assets/Scripts/General Gameplay/ScoreKeeper.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class ScoreKeeper : MonoBehaviour
 {
     [TextArea(1,10)]
-    public string BehaviorSummary = "Score system details: Points will awarded or removed throughout the battle based on the mecha's score, death count, and kill count.\n" +
+    public string SystemSummary = "Score system details: Points will awarded or removed throughout the battle based on the mecha's score, death count, and kill count.\n" +
                                     "Score points gained will be equal to the HP lost by the defender.\n" +
                                     "Deaths will remove that mecha's HP x (their death count x 0.66) from their score.\n" + 
                                     "Any death incurred after gaining the kill bonus will reset the bonus to 0%. All points gained during the killstreak will not be affected.\n" +
@@ -17,8 +17,10 @@ public class ScoreKeeper : MonoBehaviour
     [SerializeField] private TextMeshProUGUI playerRanking;
     [SerializeField] private List<GameObject> rankIndicators;
 
-    // Start is called before the first frame update
-    void Start()
+    private List<ScoreHandler> participants = new List<ScoreHandler>();
+    public List<ScoreHandler> _participants { get { return participants; } }
+
+    private void Awake()
     {
         
     }
