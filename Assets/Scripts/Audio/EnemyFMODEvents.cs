f1a36f0438593a69462a9bbf0f5ef1d12645c2ff
Alfonso Salinas <alfonsoisonido@gmail.com>
2020-02-11 15:06:26 -0600

    Implemented sound for Forcefields on mechas

---
+++ Assets/Scripts/Audio/EnemyFMODEvents.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFMODEvents : MonoBehaviour
+{
+    FMOD.Studio.EventInstance SFX_Forcefield;
+    GameObject Shield;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SFX_Forcefield = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/SFX_Forcefield");
+        Shield = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SFX_Forcefield.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        Shield = gameObject.transform.Find("Shield").gameObject;
+
+        if (Shield.activeSelf && (PlaybackState(SFX_Forcefield) != FMOD.Studio.PLAYBACK_STATE.PLAYING))
+        {
+            PlayForcefieldSound();
+        }
+
+        else if (!Shield.activeSelf && (PlaybackState(SFX_Forcefield) == FMOD.Studio.PLAYBACK_STATE.PLAYING))
+        {
+            StopForcefieldSound();
+        }
+
+    }
+
+
+    public void PlayForcefieldSound()
+    {
+        SFX_Forcefield.start();
+    }
+
+    public void StopForcefieldSound()
+    {
+        SFX_Forcefield.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    FMOD.Studio.PLAYBACK_STATE PlaybackState(FMOD.Studio.EventInstance instance)
+    {
+        FMOD.Studio.PLAYBACK_STATE pS;
+        instance.getPlaybackState(out pS);
+        return pS;
+    }
+}
--- Assets/Scripts/Audio/PlayerFMODEvents.cs
+++ Assets/Scripts/Audio/PlayerFMODEvents.cs
@@ -6,21 +6,42 @@ public class PlayerFMODEvents : MonoBehaviour
 {
     FMOD.Studio.EventInstance SFX_Dash;
     FMOD.Studio.PARAMETER_ID dashParameterID;
+
+    FMOD.Studio.EventInstance SFX_Forcefield;
+
+    GameObject Shield;
     // Start is called before the first frame update
     void Awake()
     {
+        //Dash sound
         SFX_Dash = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/SFX_Dash");
         FMOD.Studio.EventDescription dashEventDescription;
         SFX_Dash.getDescription(out dashEventDescription);
         FMOD.Studio.PARAMETER_DESCRIPTION dashParameterDescription;
         dashEventDescription.getParameterDescriptionByName("EndDash", out dashParameterDescription);
         dashParameterID = dashParameterDescription.id;
+
+        //Forcefield Sound
+        SFX_Forcefield = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/SFX_Forcefield");
+        Shield = new GameObject();
     }
 
     // Update is called once per frame
     void Update()
     {
         SFX_Dash.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        SFX_Forcefield.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        Shield = gameObject.transform.Find("Shield").gameObject;
+
+        if (Shield.activeSelf && (PlaybackState(SFX_Forcefield) != FMOD.Studio.PLAYBACK_STATE.PLAYING))
+        {
+            PlayForcefieldSound();
+        }
+
+        else if (!Shield.activeSelf && (PlaybackState(SFX_Forcefield) == FMOD.Studio.PLAYBACK_STATE.PLAYING))
+        {
+            StopForcefieldSound();
+        }
 
     }
 
@@ -28,14 +49,21 @@ public class PlayerFMODEvents : MonoBehaviour
     {
         SFX_Dash.setParameterByID(dashParameterID, 0);
         SFX_Dash.start();
-        Debug.Log("start dash sound");
-
     }
 
     public void StopDashSound()
     {
         SFX_Dash.setParameterByID(dashParameterID, 1);
-        Debug.Log("stop dash sound");
+    }
+
+    public void PlayForcefieldSound()
+    {
+        SFX_Forcefield.start();   
+    }
+
+    public void StopForcefieldSound()
+    {
+        SFX_Forcefield.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
 
     FMOD.Studio.PLAYBACK_STATE PlaybackState(FMOD.Studio.EventInstance instance)
