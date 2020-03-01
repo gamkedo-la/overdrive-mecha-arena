9d1bb81982a89ecfdfe037977c8bdddf5a3b1e9d
Jaime Rivas <jaimerivas81@outlook.com>
2020-03-01 08:57:20 -0800

    FInal results scene leads to main menu on button press

---
+++ Assets/ToMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMainMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
