bd10524c705e2d259f5a6139151e02c6bad462da
Alfonso Salinas <alfonsoisonido@gmail.com>
2020-03-02 14:59:48 -0600

    Added stop all audio events when going from final results scen to main menu

--- Assets/ToMainMenu.cs
+++ Assets/ToMainMenu.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class ToMainMenu : MonoBehaviour
 {
+    FMOD.Studio.Bus masterBus;
+
+    private void Awake()
+    {
+        masterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
+    }
     public void MainMenu()
     {
+        masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         SceneManager.LoadScene(0);
     }
 }
