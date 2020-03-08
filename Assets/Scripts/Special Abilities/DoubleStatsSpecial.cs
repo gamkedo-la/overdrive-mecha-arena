fea2d9dced66ae36b08a736c79f426e475e399d5
Jaime Rivas <jaimerivas81@outlook.com>
2020-03-07 16:46:39 -0800

    forgot to save changes to special abilities

--- Assets/Scripts/Special Abilities/DoubleStatsSpecial.cs
+++ Assets/Scripts/Special Abilities/DoubleStatsSpecial.cs
@@ -1,11 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoubleStatsSpecial : SpecialAbility
 {
+    private bool areStatsBuffed = false;
+    public bool _areStatsBuffed { get { return areStatsBuffed; } }
+
     protected override void Update()
     {
         base.Update();
+
+        if (CanUseSpecial())
+        {
+            if (Input.GetButtonDown("Fire3") && gameObject.tag == "Player")
+            {
+                UseDoubleStatsSpecial();
+            }
+        }
+        if (!isSpecialInUse)
+        {
+            areStatsBuffed = isSpecialInUse;
+        }
+    }
+
+    public void UseDoubleStatsSpecial()
+    {
+        specialCooldownTimer = 0.0f;
+        isSpecialInUse = true;
+        areStatsBuffed = isSpecialInUse;
     }
 }
